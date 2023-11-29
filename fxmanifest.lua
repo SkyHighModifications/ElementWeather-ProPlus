98a221989cf717d9d978fa8cb9584852d17b0b4c
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-11-29 23:28:58 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,20 +5,9 @@
 fx_version "cerulean"
 game "gta5"
 description "FiveM WeatherSync ProPlus"
-version "1.0.0"
 version_2 "3.2.0"
 author "SkyHigh Modifications / @Papa Smurf / "
 
 server_scripts {
     'server/*.lua'
-}
-
-client_scripts {
-    'client/*.lua'
-}
-
-shared_script "config.lua"
-
-escrow_ignore {
-    "config.lua"
 }
